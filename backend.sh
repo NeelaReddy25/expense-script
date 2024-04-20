d76e7099c00250e57f8b58e9e09190f4db311401
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-20 17:37:10 +0530

    Good somewhere

--- backend.sh
+++ backend.sh
@@ -76,7 +76,7 @@ VALIDATE $? "Enabling backend"
 dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MYSQL"
 
-mysql -h 172.31.20.165 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+mysql -h db.neelareddy.store -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
 VALIDATE $? "Schema loading"
 
 systemctl restart backend &>>$LOGFILE
